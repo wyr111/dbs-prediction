c723bbb454bf02d0a463ed58e41b1fb9a29765c3
wyr111 <108868326+wyr111@users.noreply.github.com>
2022-07-13 18:27:29 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, render_template, request
+
+
+# In[2]:
+
+
+import joblib
+
+
+# In[3]:
+
+
+app = Flask(__name__)
+
+
+# In[4]:
+
+
+@app.route("/", methods =["GET", "POST"])
+def index():
+    if request.method == "POST":
+        rates = float(request.form.get("rates"))
+        print(rates)
+        model = joblib.load("regression")
+        pred = model.predict([[rates]])
+        return(render_template("index.html", result = pred ))
+    else:
+        return(render_template("index.html", result = "WAITING"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
